40346bec17a348ba4a9ba4955793d3307a270c77
Ricardoalmeida54 <ricardobotafoguense20@gmail.com>
2021-06-13 12:32:42 -0300

    backend site react
    BAIXOU A API SO DA UM NPM I PARA INSTALAR OS PACOTES

---
+++ src/Models/Product.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const PointSchema = require('./Utils/PontSchema')
+
+const Schema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+        },
+        location: {
+            type: PointSchema,
+            index: '2dsphere',
+        },
+        order: {
+            type: Number,
+            
+        }
+})
+
+module.exports = mongoose.model('Product', Schema)
